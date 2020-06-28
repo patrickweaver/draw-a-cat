f31c7755abd4fb67c0326702e4cf036a306ddfe9
Glitch (draw-a-cat) <none>
2020-06-28 20:09:26 +0000

    Implemented websockets to join someone else's room

--- public/script.js
+++ public/script.js
@@ -1,38 +1,122 @@
+// State variables:
+var roomCode = null;
+var roomSound = 1;
+var currentCat = null;
+var currentSeconds = null;
+
+// DOM elements:
 const cat = document.getElementById("cat");
 const countdown = document.getElementById("countdown");
+const roomCodeSpan = document.getElementById("room-code");
+const roomCodeInput = document.getElementById("room-code-input");
+
+/* - - - - - - - - - -
+   Setup Websocket:
+  - - - - - - - - - - */
+
+// Match websocket protocol to page protocol (ws/http or wss/https):
+var wsProtocol = window.location.protocol == "https:" ? "wss" : "ws";
+
+// Set up new websocket connection to server
+var connection = new WebSocket(`${wsProtocol}://${window.location.hostname}`);
+
+// Log successful connection
+connection.onopen = function() {
+  console.log("Websocket connected!");
+  
+  // Auto generate new room on new connection
+  createRoom();
+};
+
+// Set this function to run every time the websocket receives a message from the server:
+// Each message will have data that represents a player that has moved.
+connection.onmessage = function(message) {
+  
+  const parsedMessageData = JSON.parse(message.data);
+  console.log("Parsed Message Data:");
+  console.log(parsedMessageData);
+  
+  if (parsedMessageData.type === "newCat") {
+     if (parsedMessageData.catData && parsedMessageData.seconds) {
+       showNewCat(parsedMessageData.catData, parsedMessageData.seconds);
+     } 
+  }
+  
+  if (parsedMessageData.type === "requestCurrentCat") {
+    if (parsedMessageData.joinCode) {
+      sendCurrentCat(parsedMessageData.joinCode)
+    }
+  }
+};
+
+function createRoom() {
+  roomCode = Math.random().toString(36).substr(2, 6);
+  roomCodeSpan.innerHTML = roomCode;
+  roomSound = Math.ceil(Math.random() * 7);
+  connection.send(
+    JSON.stringify({
+      type: "newRoom",
+      roomCode: roomCode,
+      roomSound: roomSound
+    })
+  );
+}
+
+function requestJoinRoom() {
+  roomCode = roomCodeInput.value;
+  roomCodeSpan.innerHTML = roomCode;
+  connection.send(
+    JSON.stringify({
+      type: "requestJoinRoom",
+      newRoomCode: roomCode
+    })
+  );
+}
+
+function requestNewCat() {
+  connection.send(
+    JSON.stringify({
+      type: "requestNewCat",
+      roomCode: roomCode
+    })
+  )
+}
+
+function sendCurrentCat(joinCode) {
+  connection.send(
+    JSON.stringify({
+      type: "sendCurrentCat",
+      joinCode: joinCode,
+      catData: {url: currentCat},
+      seconds: currentSeconds
+    })
+  )
+}
 
 
-async function newCat() {
+async function showNewCat(catData, seconds) {
+  
+  console.log("showNewCat()");
+  console.log(catData);
+  console.log(seconds);
   
   try {
-  
-    const catDataResponse = await fetch("https://api.thecatapi.com/v1/images/search");
-    const catData = await catDataResponse.json();
-    console.log(catData[0]);
-
-    if (catData[[0]]) {
-      cat.innerHTML = `<img class="cat-image" src=${catData[0].url} />`;
-    }
-    
-    
-    let seconds = Math.floor(Math.random() * 240) + 60;
-    
-    countdown.innerHTML = `<span id="seconds-remaining">${seconds}</span> seconds left!`
+    if (catData) {
+      cat.innerHTML = `<img class="cat-image" src=${catData.url} />`;
+      currentCat = catData.url;
+    }   
+    countdown.innerHTML = `<span id="seconds-remaining">${seconds}</span> seconds remaining`
     const secondsRemaining = document.getElementById("seconds-remaining");
     
     setInterval(() => {
       seconds--;
       secondsRemaining.innerHTML = seconds;
+      currentSeconds = seconds;
     }, 1000)
-    
-    
-    
+   
   } catch (error) {
     cat.innerHTML = "";
+    console.log("Error in showNewCat()");
     console.log(error);
   }
-  
 }
-
-
-newCat();
--- server.js
+++ server.js
@@ -1,18 +1,135 @@
-
+const https = require('https');
 const express = require("express");
 const app = express();
 app.use(express.static("public"));
 
 
-app.get("/api", (req, res) => {
-  
-  
-  
-})
+// No dependency request like function
+function gets(url) {
+  return new Promise((resolve, reject) => {
+   https.get(url, (response) => {
+   let body = ''
+   response.on('data', (chunk) => body += chunk)
+   response.on('end', () => resolve(body))
+   }).on('error', reject)
+  })
+}
 
-
+async function newCat() {
+  const url = "https://api.thecatapi.com/v1/images/search?mime_types=jpg,png";
+  const api = () => gets(url);
+  const cat = JSON.parse(await api());
+  if (cat[0]) {
+    return cat[0];
+  } else {
+    return null;
+  }
+}
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+const server = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + server.address().port);
 });
+
+
+// Websocket Server:
+// We are using the external library 'ws' to set up the websockets on the server
+// https://www.npmjs.com/package/ws
+// In our code this is stored in the variable WebSocket.
+var WebSocket = require("ws");
+
+// Connect our Websocket server to our server variable to serve requests on the same port:
+var wsServer = new WebSocket.Server({ server });
+
+// This function will send a message to all clients connected to the websocket:
+function broadcast(roomCode, data) {
+  wsServer.clients.forEach(client => {
+    if (client.roomCode === roomCode && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(data));
+    }
+  });
+}
+
+// This outer function will run each time the Websocket
+// server connects to a new client:
+wsServer.on("connection", ws => {
+  ws.id = 'id-' + Math.random().toString(36).substr(2, 6);
+  ws.roomCode = null;
+  ws.roomSound = null;
+
+  // This function will run every time the server recieves a message with that client.
+  ws.on("message", async data => {
+
+    console.log("\nMessage Received from ", ws.id);
+    console.log(data);
+    
+    const messageData = JSON.parse(data)
+    
+    ws.roomCode = messageData.roomCode ? messageData.roomCode : ws.roomCode;
+    ws.roomSound = messageData.roomSound ? messageData.rooMSound : ws.roomSound;
+    
+    switch(messageData.type) {
+      case "requestNewCat":
+        requestNewCat(ws);
+        break;
+      case "requestJoinRoom":
+        requestJoinRoom(ws, messageData);
+        break;
+      case "sendCurrentCat":
+        sendCurrentCat(messageData);
+        break;
+      default:
+        console.log("Invalid message type:", messageData.type);
+    }
+    
+  });
+
+  ws.on("close", () => {
+    console.log("Disconnected:", ws.id);
+    // Here you could send a message to other clients that
+    // this client has disconnected.
+  });
+});
+
+
+async function requestNewCat(ws) {
+  const newCatData = await newCat()
+      
+  console.log("** New Cat Data:");
+  console.log(newCatData);
+
+
+  const seconds = Math.floor(Math.random() * 170) + 30;
+  broadcast(ws.roomCode, {
+    type: "newCat",
+    catData: newCatData,
+    seconds: seconds
+  })
+}
+
+function requestJoinRoom(ws, messageData) {
+  ws.joinCode = Math.random().toString(36).substr(2, 6);
+  wsServer.clients.forEach(client => {
+    if (client.roomCode === messageData.newRoomCode && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({
+        type: "requestCurrentCat",
+        joinCode: ws.joinCode
+      }));
+    }
+  });
+  //
+  ws.roomCode = messageData.newRoomCode;
+}
+
+function sendCurrentCat(messageData) {
+  wsServer.clients.forEach(client => {
+    if (client.joinCode === messageData.joinCode && client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify({
+        type: "newCat",
+        catData: messageData.catData,
+        seconds: messageData.seconds,
+        roomSound: messageData.roomSound
+      }));
+    }
+  });
+}
